a912a9e69e6a892d1c4772fa88237817aaa4d567
aarystanovv <ttt.arystan@gmail.com>
2025-09-14 22:02:05 +0500

    [FIX] fix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from app.rented_cache import fetch_rented_plates
 from app.router import router
 from app.alerts import process_vehicle_notifications
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("httpcore").setLevel(logging.WARNING)
